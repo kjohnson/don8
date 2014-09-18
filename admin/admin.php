3881f99a8ca0a0f2099105a4cba0f192b2e8a6e4
Kyle Maurer <kyle@realbigmarketing.com>
2014-09-18 15:44:09 -0400

    Some simple code cleanup.

--- admin/admin.php
+++ admin/admin.php
@@ -3,46 +3,58 @@
 * Creates the admin settings page and the options for the plugin
 * tut here: http://www.wphub.com/tutorials/creating-simple-options-page/
 */
-add_action("admin_menu","don8_settings_page");
+class don8Settings {
 
-function don8_settings_page() {
-	add_submenu_page( 'options-general.php', 'Don8 Options', 'Don8', 'manage_options', 'don8', 'don8_settings_body' );
-}
-function don8_register_settings() {
-	add_option( 'don8_paypal_email', '');
-	add_option( 'don8_cause', '');
-	add_option( 'don8_value', '');
-	register_setting( 'don8_settings', 'don8_paypal_email' ); 
-	register_setting( 'don8_settings', 'don8_cause' );
-	register_setting( 'don8_settings', 'don8_value' );
-} 
-add_action( 'admin_init', 'don8_register_settings' );
+	private $pre = 'don8';
 
-function don8_settings_body() { ?>
-<div class="wrap">
-	<?php screen_icon(); ?>
-	<h2>Don8 Options</h2>
-	<form method="post" action="options.php"> 
-		<?php settings_fields( 'don8_settings' ); ?>
-		<h3>Configuration</h3>
-			<p>Here you can set the default values for your donation button.</p>
-			<table class="form-table">
-				<tr valign="top">
-					<th scope="row"><label for="don8_paypal_email">Paypal e-mail</label></th>
-					<td><input type="text" id="don8_paypal_email" name="don8_paypal_email" value="<?php echo get_option('don8_paypal_email'); ?>" /></td>
-				</tr>
-				<tr valign="top">
-					<th scope="row"><label for="don8_cause">Cause</label></th>
-					<td><input type="text" id="don8_cause" name="don8_cause" value="<?php echo get_option('don8_cause'); ?>" /></td>
-				</tr>
-				<tr valign="top">
-					<th scope="row"><label for="don8_value">Amount</label></th>
-					<td><input type="text" id="don8_value" name="don8_value" value="<?php echo get_option('don8_value'); ?>" /></td>
-				</tr>
-			</table>
-		<?php submit_button(); ?>
-	</form>
-</div>
-<?php
+	public function __construct() {
+	add_action('admin_menu','settings_page');
+	add_action( 'admin_init', 'register_settings' );
+	}
+	public function settings_page() {
+		add_submenu_page( 'options-general.php',
+			'Don8 Options',
+			'Don8',
+			'manage_options',
+			'don8',
+			array( $this, 'body' )
+		);
+	}
+	public function register_settings() {
+		add_option( 'don8_paypal_email', '');
+		add_option( 'don8_cause', '');
+		add_option( 'don8_value', '');
+		register_setting( 'don8_settings', 'don8_paypal_email' );
+		register_setting( 'don8_settings', 'don8_cause' );
+		register_setting( 'don8_settings', 'don8_value' );
+	}
+
+	public function body() { ?>
+	<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2>Don8 Options</h2>
+		<form method="post" action="options.php">
+			<?php settings_fields( 'don8_settings' ); ?>
+			<h3>Configuration</h3>
+				<p>Here you can set the default values for your donation button.</p>
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row"><label for="don8_paypal_email">Paypal e-mail</label></th>
+						<td><input type="text" id="don8_paypal_email" name="don8_paypal_email" value="<?php echo get_option('don8_paypal_email'); ?>" /></td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="don8_cause">Cause</label></th>
+						<td><input type="text" id="don8_cause" name="don8_cause" value="<?php echo get_option('don8_cause'); ?>" /></td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="don8_value">Amount</label></th>
+						<td><input type="text" id="don8_value" name="don8_value" value="<?php echo get_option('don8_value'); ?>" /></td>
+					</tr>
+				</table>
+			<?php submit_button(); ?>
+		</form>
+	</div>
+	<?php
+	}
 }
-?>
+$don8 = new don8Settings();
--- functions/form.php
+++ functions/form.php
@@ -1,26 +1,28 @@
 <?php
 // HTML form
 // Code from here https://www.paypal.com/uk/cgi-bin/webscr?cmd=_pdn_donate_techview_outside
-function don8_form($atts, $content = null) {
+function don8_form( $atts, $content = null ) {
 // Get values from Don8 options
-$don8_email = get_option('don8_paypal_email');
-$don8_cause = esc_attr(get_option('don8_cause'));
-$don8_value = esc_attr(get_option('don8_value'));
+	$don8_email = get_option( 'don8_paypal_email' );
+	$don8_cause = esc_attr( get_option( 'don8_cause' ) );
+	$don8_value = esc_attr( get_option( 'don8_value' ) );
 // Set Don8 options and defaults for parameters
-  extract(shortcode_atts(array(
-     'cause' => $don8_cause,
-     'email' => $don8_email,
-     'value' => $don8_value,
-    ), $atts));
+	extract( shortcode_atts( array(
+		'cause' => $don8_cause,
+		'email' => $don8_email,
+		'value' => $don8_value,
+	), $atts ) );
 
-$don8_form = '<form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
+	$don8_form = '<form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_xclick">
-<input type="hidden" name="business" value="'.$email.'">
-<input type="hidden" name="item_name" value="'.$cause.'">
+<input type="hidden" name="business" value="' . $email . '">
+<input type="hidden" name="item_name" value="' . $cause . '">
 <input type="hidden" name="currency_code" value="GBP">
-<input type="hidden" name="amount" value="'.$value.'">
-<input type="image" src="http://www.paypal.com/en_GB/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Donate to '.$cause.'">
+<input type="hidden" name="amount" value="' . $value . '">
+<input type="image" src="http://www.paypal.com/en_GB/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Donate to ' . $cause . '">
 </form>';
-return $don8_form;
+
+	return $don8_form;
 }
-?>
+
+add_shortcode( 'don8', 'don8_form' );
