c97d5438aca9bd3d5b476c36031b7e5c0dc0e979
Kyle Maurer <kyle@realbigmarketing.com>
2014-09-25 17:15:31 -0400

    Moved shortcodes into class. Added currency option and parameter. Added some more excaping.

--- admin/admin.php
+++ admin/admin.php
@@ -7,8 +7,6 @@
 
 class don8Settings {
 
-	private $pre = 'don8';
-
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'settings_page' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
@@ -27,9 +25,11 @@ class don8Settings {
 	public function register_settings() {
 		add_option( 'don8_paypal_email', '' );
 		add_option( 'don8_cause', '' );
+		add_option( 'don8_currency', '' );
 		add_option( 'don8_value', '' );
 		register_setting( 'don8_settings', 'don8_paypal_email' );
 		register_setting( 'don8_settings', 'don8_cause' );
+		register_setting( 'don8_settings', 'don8_currency' );
 		register_setting( 'don8_settings', 'don8_value' );
 	}
 
@@ -41,7 +41,8 @@ class don8Settings {
 
 			<form method="post" action="options.php">
 				<?php settings_fields( 'don8_settings' );
-				$email = get_option( 'don8_paypal_email' );
+				$email    = get_option( 'don8_paypal_email' );
+				$currency = get_option( 'don8_currency' );
 				?>
 				<h3>Donation default values</h3>
 
@@ -61,6 +62,15 @@ class don8Settings {
 						<th scope="row"><label for="don8_cause">Cause</label></th>
 						<td><input type="text" id="don8_cause" name="don8_cause"
 						           value="<?php echo get_option( 'don8_cause' ); ?>"/></td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="don8_currency">Currency Code</label></th>
+						<td><input type="text" id="don8_currency" name="don8_currency"
+						           value="<?php if ( ! empty( $currency ) ) {
+							           echo $currency;
+						           } else {
+							           echo 'USD';
+						           } ?>"/></td>
 					</tr>
 					<tr valign="top">
 						<th scope="row"><label for="don8_value">Amount</label></th>
--- functions/form.php
+++ functions/form.php
@@ -1,19 +1,68 @@
 <?php
-// HTML form
+// PayPal Button
 // Code from here https://www.paypal.com/uk/cgi-bin/webscr?cmd=_pdn_donate_techview_outside
-function don8_button( $atts, $content = null ) {
-// Get values from Don8 options
-	$don8_email = get_option( 'don8_paypal_email' );
-	$don8_cause = esc_attr( get_option( 'don8_cause' ) );
-	$don8_value = esc_attr( get_option( 'don8_value' ) );
-// Set Don8 options and defaults for parameters
-	extract( shortcode_atts( array(
-		'cause' => $don8_cause,
-		'email' => $don8_email,
-		'value' => $don8_value,
-	), $atts ) );
+class don8 {
 
-	$don8_button = '<form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
+	public function __construct() {
+		add_shortcode( 'don8', array( $this, 'don8' ) );
+		add_shortcode( 'don8_button', array( $this, 'button' ) );
+		add_shortcode( 'don8_form', array( $this, 'form' ) );
+	}
+
+	public function don8( $atts, $content = null ) {
+		// Get values from Don8 options
+		$don8_email = get_option( 'don8_paypal_email' );
+		$don8_cause = esc_attr( get_option( 'don8_cause' ) );
+		$don8_currency = esc_attr( get_option( 'don8_currency' ) );
+		$don8_value = esc_attr( get_option( 'don8_value' ) );
+// Set Don8 options and defaults for parameters
+		extract( shortcode_atts( array(
+			'cause' => $don8_cause,
+			'email' => $don8_email,
+			'currency' => $don8_currency,
+			'value' => $don8_value,
+		), $atts ) );
+
+		$don8_button = '<form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="' . esc_html( $email ) . '">
+<input type="hidden" name="item_name" value="' . esc_html( $cause ) . '">
+<input type="hidden" name="currency_code" value="' . esc_html( $currency ) . '">
+<input type="hidden" name="amount" value="' . esc_html( $value ) . '">
+<input type="image" src="http://www.paypal.com/en_GB/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Donate to ' . esc_html( $cause ) . '">
+</form>';
+
+		return $don8_button;
+	}
+
+	public function button( $atts, $content = null ) {
+
+	}
+
+	public function form( $atts, $content = null ) {
+
+	}
+
+}
+
+new don8();
+/**
+ * [don8]
+ * [don8_button]
+ * [don8_form]
+ */
+// Get values from Don8 options
+$don8_email = get_option( 'don8_paypal_email' );
+$don8_cause = esc_attr( get_option( 'don8_cause' ) );
+$don8_value = esc_attr( get_option( 'don8_value' ) );
+// Set Don8 options and defaults for parameters
+extract( shortcode_atts( array(
+	'cause' => $don8_cause,
+	'email' => $don8_email,
+	'value' => $don8_value,
+), $atts ) );
+
+$don8_button = '<form name="_xclick" action="https://www.paypal.com/uk/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_xclick">
 <input type="hidden" name="business" value="' . $email . '">
 <input type="hidden" name="item_name" value="' . $cause . '">
@@ -22,7 +71,4 @@ function don8_button( $atts, $content = null ) {
 <input type="image" src="http://www.paypal.com/en_GB/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Donate to ' . $cause . '">
 </form>';
 
-	return $don8_button;
-}
-
-add_shortcode( 'don8_button', 'don8_button' );
+return $don8_button;
