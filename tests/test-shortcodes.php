4aaf7c3c1d995acec42d118c9d2c6f0a31a49b63
Kyle Maurer <kyle@realbigmarketing.com>
2014-12-31 12:09:59 -0500

    Experimenting with TravisCI and unit tests. Modeled after EDD.

---
+++ tests/test-shortcodes.php
@@ -0,0 +1,6 @@
+<?php
+
+class Tests_Shortcode extends WP_UnitTestCase {
+	$test = 1;
+	$this->assertContains( 1, $this->test );
+}
---
+++ travis-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
+then
+  phpunit
+else
+  phpunit --coverage-clover=coverage.clover
+fi
